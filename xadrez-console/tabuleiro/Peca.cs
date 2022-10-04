8ac3bf8231c9bee8f9c1bde84c2d2ad8cdb922aa
Tulio Gabriel <tulioloks@hotmail.com>
2022-10-04 12:05:51 -0300

    Implementando as resticoes de movimento

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -5,10 +5,10 @@
         public Posicao posicao { get; set; }
         public Cor cor { get; protected set; }
         public int qteMovimentos { get; protected set; }    
-        public Tabuleiro tab { get; protected set; }
+        public Tabuleiro tab { get; protected set; }               
 
         public Peca (Tabuleiro tab, Cor cor)
-        {
+        {                                                               
             
             this.posicao = null;
             this.tab = tab;
@@ -18,6 +18,26 @@
 
         public abstract bool[,] movimentosPossiveis();
        
+        public bool existeMovimentosPossiveis()
+        {
+            bool[,] mat = movimentosPossiveis();
+            for (int i = 0; i < tab.linhas; i++)
+            {
+                for(int j =0; j < tab.colunas; j++)
+                {
+                    if (mat[i, j])
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        public bool podeMoverPara(Posicao pos)
+        {
+            return movimentosPossiveis()[pos.linha, pos.coluna];
+        }
 
         public void incrementarQtdMovimentos()
         {
