6aee2d06c45c9cad6af2c7eed9e129134a1d67ec
Tulio Gabriel <tulioloks@hotmail.com>
2022-10-10 17:01:11 -0300

    Jogada especial promocao

--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -141,6 +141,18 @@ namespace xadrez
                 throw new TabuleiroException("Voce não pode se colocar em xeque!");
             }
 
+            Peca p = tab.peca(destino);
+            //#jogadaEspecial Promoção
+            if (p is Peao){
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)){
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -158,8 +170,6 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
 
             //#jogadaEspecial En Passant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha +2))
